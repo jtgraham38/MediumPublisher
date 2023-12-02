8d6bf0b2223dc4889cf26fed78df9353135453c9
Jacob <jtgraham38@gmail.com>
2023-12-01 23:43:06 -0500

    indent change

--- elements/settings/setup_instructions.php
+++ elements/settings/setup_instructions.php
@@ -8,15 +8,15 @@ if (!defined('ABSPATH')) {
 <details>
     <summary>Setup Instructions</summary>
     <small>
-    <ol>
-        <li>Register/Login to a <a href="https://medium.com/">Medium Account</a>.</li>
-        <li>Navigate to your Profile</li>
-        <li>Click "Edit Profile"</li>
-        <li>Click "Security and Apps"</li>
-        <li>Click "Integration Tokens"</li>
-        <li>Create an integration token, and name it something descriptive, like "Wordpress Token".</li>
-        <li>Copy the token, and paste it into the "Medium Token" field.</li>
-        <li>Save and test to ensure your account details are gotten successfully.</li>
-    </ol>
-</small>
+        <ol>
+            <li>Register/Login to a <a href="https://medium.com/">Medium Account</a>.</li>
+            <li>Navigate to your Profile</li>
+            <li>Click "Edit Profile"</li>
+            <li>Click "Security and Apps"</li>
+            <li>Click "Integration Tokens"</li>
+            <li>Create an integration token, and name it something descriptive, like "Wordpress Token".</li>
+            <li>Copy the token, and paste it into the "Medium Token" field.</li>
+            <li>Save and test to ensure your account details are gotten successfully.</li>
+        </ol>
+    </small>
 </details>
