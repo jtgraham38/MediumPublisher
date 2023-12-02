30a9d2820c2a01d93fad6407c9035d0684cac5ce
Jacob <jtgraham38@gmail.com>
2023-12-01 23:41:43 -0500

    Added instructions.

---
+++ elements/settings/setup_instructions.php
@@ -0,0 +1,22 @@
+<?php 
+// exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+?>
+<p>Configure settings for Medium Publisher!</p>
+<details>
+    <summary>Setup Instructions</summary>
+    <small>
+    <ol>
+        <li>Register for/Login to a <a href="https://medium.com/">Medium Account</a>.</li>
+        <li>Navigate to your Profile</li>
+        <li>Click "Edit Profile"</li>
+        <li>Click "Security and Apps"</li>
+        <li>Click "Integration Tokens"</li>
+        <li>Create an integration token, and name it something descriptive, like "Wordpress Token".</li>
+        <li>Copy the token, and paste it into the "Medium Token" field.</li>
+        <li>Save and test to ensure your account details are gotten successfully.</li>
+    </ol>
+</small>
+</details>
