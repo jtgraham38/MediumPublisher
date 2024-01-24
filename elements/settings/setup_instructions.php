74f2d5d701aa4a3f88f6688a5d04a5e4af5e26f8
Jacob <jtgraham38@gmail.com>
2024-01-24 11:56:46 -0500

    Good punctuation.

--- elements/settings/setup_instructions.php
+++ elements/settings/setup_instructions.php
@@ -10,10 +10,10 @@ if (!defined('ABSPATH')) {
     <small>
         <ol>
             <li>Register/Login to a <a href="https://medium.com/">Medium Account</a>.</li>
-            <li>Navigate to your Profile</li>
-            <li>Click "Edit Profile"</li>
-            <li>Click "Security and Apps"</li>
-            <li>Click "Integration Tokens"</li>
+            <li>Navigate to your Profile.</li>
+            <li>Click "Edit Profile".</li>
+            <li>Click "Security and Apps".</li>
+            <li>Click "Integration Tokens".</li>
             <li>Create an integration token, and name it something descriptive, like "Wordpress Token".</li>
             <li>Copy the token, and paste it into the "Medium Token" field.</li>
             <li>Save and test to ensure your account details are gotten successfully.</li>
